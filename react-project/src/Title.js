a17393703feb4f94684a603a2245251c6b789299
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-07-10 20:25:39 -0700

    header component built

---
+++ react-project/src/Title.js
@@ -0,0 +1,15 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div classname="Title">
+        <h1>Mood Chart</h1>
+        <div className="Title-Subtitle">How do you feel today?</div>          
+    </div>
+  )
+}
+
+export default Title
